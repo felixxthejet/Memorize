bb5792d5c955d34b0005aa5f362ec51aff932158
Felix Shen <shenzoutong@gmail.com>
2023-11-07 20:47:47 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Felix Shen on 11/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding()
+        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
